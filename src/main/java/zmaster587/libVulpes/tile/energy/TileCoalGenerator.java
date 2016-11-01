1b81fa30ea27b077c3c4693518bd0a914f4f5267
zmaster587 <zmasterfun@gmail.com>
2016-11-01 15:04:14 -0400

    Added iridium

--- src/main/java/zmaster587/libVulpes/tile/energy/TileCoalGenerator.java
+++ src/main/java/zmaster587/libVulpes/tile/energy/TileCoalGenerator.java
@@ -17,32 +17,32 @@ public class TileCoalGenerator extends TileInventoriedForgePowerMachine {
 	int powerPerTick;
 	private static final int INPUT_SLOT = 0;
 	ModuleText textModule;
-	
+
 	public TileCoalGenerator() {
 		super(10000, 1);
 		powerPerTick = 40;
 		textModule = new ModuleText(40, 20, "Generating 0 RF/t", 0x2b2b2b);
 	}
-	
+
 	@Override
 	public List<ModuleBase> getModules(int ID, EntityPlayer player) {
 		List<ModuleBase> modules = super.getModules(ID, player);
-		
+
 		modules.add(new ModuleSlotArray(40, 40, this, 0, 1));
 		modules.add(new ModuleProgress(80, 40, 1, TextureResources.progressGenerator, this));
 		modules.add(textModule);
-		
+
 		return modules;
 	}
 
 	@Override
 	public void setInventorySlotContents(int slot, ItemStack stack) {
 		super.setInventorySlotContents(slot, stack);
-		
+
 		if(!canGeneratePower())
 			setTimeAndAmounts();
 	}
-	
+
 	@Override
 	public int getPowerPerOperation() {
 		return powerPerTick;
@@ -56,7 +56,8 @@ public class TileCoalGenerator extends TileInventoriedForgePowerMachine {
 
 	private void setTimeAndAmounts() {
 		timeRemaining = TileEntityFurnace.getItemBurnTime(inventory.getStackInSlot(INPUT_SLOT));
-		inventory.decrStackSize(INPUT_SLOT, 1);
+		if(timeRemaining > 0)
+			inventory.decrStackSize(INPUT_SLOT, 1);
 	}
 
 	@Override
