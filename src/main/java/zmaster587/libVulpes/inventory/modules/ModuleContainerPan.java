753909b2f2acf5902944afa7e63b451b1953bfdd
zmaster587 <zmasterfun@gmail.com>
2017-03-05 16:42:18 -0500

    Fix containerPan

--- src/main/java/zmaster587/libVulpes/inventory/modules/ModuleContainerPan.java
+++ src/main/java/zmaster587/libVulpes/inventory/modules/ModuleContainerPan.java
@@ -331,7 +331,7 @@ public class ModuleContainerPan extends ModuleBase {
 
 		if(backdrop != null) {
 			gui.mc.getTextureManager().bindTexture(backdrop);
-			gui.drawTexturedModalRect(x + offsetX, y + offsetY, (int)(-0.1*currentPosX), (int)(-0.1*currentPosY), screenSizeX, screenSizeY);
+			gui.drawTexturedModalRect(x + offsetX, y + offsetY, (int)(-0.1*currentPosX), (int)(-0.1*currentPosY), offsetX + screenSizeX,  offsetY + screenSizeY);
 		}
 		
 		for(GuiButton button : buttonList)
