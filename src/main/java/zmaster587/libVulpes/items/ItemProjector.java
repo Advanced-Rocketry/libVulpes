3eb8003764293e297071aef0564939ea883707f7
zmaster587 <zmasterfun@gmail.com>
2017-04-02 01:42:52 -0400

    Fix 1.10 import

--- src/main/java/zmaster587/libVulpes/items/ItemProjector.java
+++ src/main/java/zmaster587/libVulpes/items/ItemProjector.java
@@ -41,8 +41,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.MouseEvent;
