383572ea0c8b30a1b6872c81264427a0797cc161
zmaster587 <zmasterfun@gmail.com>
2017-09-15 22:10:15 -0400

    fix https://github.com/zmaster587/AdvancedRocketry/issues/775

--- src/main/java/zmaster587/libVulpes/inventory/modules/ModuleSlotArmor.java
+++ src/main/java/zmaster587/libVulpes/inventory/modules/ModuleSlotArmor.java
@@ -2,10 +2,19 @@ package zmaster587.libVulpes.inventory.modules;
 
 import java.util.List;
 
+import javax.annotation.Nonnull;
+
 import zmaster587.libVulpes.inventory.ContainerModular;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.SlotItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
+import net.minecraftforge.items.wrapper.PlayerArmorInvWrapper;
+import net.minecraftforge.items.wrapper.RangedWrapper;
 
 public class ModuleSlotArmor extends ModuleBase {
 
@@ -16,8 +25,8 @@ public class ModuleSlotArmor extends ModuleBase {
 	public ModuleSlotArmor(int offsetX, int offsetY, EntityPlayer player) {
 		super(offsetX, offsetY);
 		this.player = player;
-		startSlot = 36;
-		endSlot = 40;
+		startSlot = 0;
+		endSlot = 4;
 	}
 
 	public void setSlotBounds(int a, int b) {
@@ -45,11 +54,50 @@ public class ModuleSlotArmor extends ModuleBase {
 			Slot slot;
 			final int k = (endSlot - startSlot) - i - 1;
 			
-			slot = new Slot(player.inventory, k + startSlot, offsetX + 18 * (i / 9), offsetY + 18*(i%9));
+			PlayerArmorInvWrapper handler = new PlayerArmorInvWrapper(player.inventory);
+			
+			slot = new SlotItemHandler(handler, k + startSlot, offsetX + 18 * (i / 9), offsetY + 18*(i%9));
 
 
 			slotList.add(slot);
 		}
 		return slotList;
 	}
+}
+
+class PlayerCustomArmorInvWrapper  extends RangedWrapper
+{
+    private final InventoryPlayer inventoryPlayer;
+
+    public PlayerCustomArmorInvWrapper(InventoryPlayer inv)
+    {
+        super(new InvWrapper(inv), inv.mainInventory.size(), 2*inv.mainInventory.size() + inv.armorInventory.size());
+        inventoryPlayer = inv;
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate)
+    {
+        EntityEquipmentSlot equ = null;
+        for (EntityEquipmentSlot s : EntityEquipmentSlot.values())
+        {
+            if (s.getSlotType() == EntityEquipmentSlot.Type.ARMOR && s.getIndex() + 36 == slot)
+            {
+                equ = s;
+                break;
+            }
+        }
+        // check if it's valid for the armor slot
+        if (equ != null && slot < 40 && !stack.isEmpty() && stack.getItem().isValidArmor(stack, equ, getInventoryPlayer().player))
+        {
+            return super.insertItem(slot, stack, simulate);
+        }
+        return stack;
+    }
+
+    public InventoryPlayer getInventoryPlayer()
+    {
+        return inventoryPlayer;
+    }
 }
