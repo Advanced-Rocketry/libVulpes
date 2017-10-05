441f3ba0e92ac10fa6bd00c4c65c6a50a7676b85
Thutmose <elpatricimo@gmail.com>
2017-10-04 20:10:38 -0400

    fixes some issues with inventories resulting from null itemstacks.
    the satilite builder issue was related to an itemstack being EMPTY
    instead of null, and the inventory was using null checks instead of
    empty checks.

--- src/main/java/zmaster587/libVulpes/util/EmbeddedInventory.java
+++ src/main/java/zmaster587/libVulpes/util/EmbeddedInventory.java
@@ -1,32 +1,32 @@
 package zmaster587.libVulpes.util;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 
 public class EmbeddedInventory implements ISidedInventory {
 
-		protected ItemStack inv[];
+		protected NonNullList<ItemStack> inv;
 		
 		public EmbeddedInventory(int size) {
-			inv = new ItemStack[size];
+			inv = NonNullList.withSize(size, ItemStack.EMPTY);
 		}
 		
 		public void writeToNBT(NBTTagCompound nbt) {
 
-			nbt.setInteger("size", inv.length);
+			nbt.setInteger("size", inv.size());
 
 			NBTTagList list = new NBTTagList();
-			for(int i = 0; i < inv.length; i++)
+			for(int i = 0; i < inv.size(); i++)
 			{
-				ItemStack stack = inv[i];
+				ItemStack stack = inv.get(i);
 
-				if(stack != null) {
+				if(!stack.isEmpty()) {
 					NBTTagCompound tag = new NBTTagCompound();
 					tag.setByte("Slot", (byte)(i));
 					stack.writeToNBT(tag);
@@ -39,46 +39,46 @@ public class EmbeddedInventory implements ISidedInventory {
 
 		public void readFromNBT(NBTTagCompound nbt) {
 			NBTTagList list = nbt.getTagList("outputItems", 10);
-			inv = new ItemStack[Math.max(nbt.getInteger("size") == 0 ? 4 : nbt.getInteger("size"), inv.length)];
+			inv = NonNullList.withSize(Math.max(nbt.getInteger("size") == 0 ? 4 : nbt.getInteger("size"), inv.size()), ItemStack.EMPTY);
 
 			for (int i = 0; i < list.tagCount(); i++) {
 				NBTTagCompound tag = (NBTTagCompound) list.getCompoundTagAt(i);
 				byte slot = tag.getByte("Slot");
-				if (slot >= 0 && slot < inv.length) {
-					inv[slot] = new ItemStack(tag);
+				if (slot >= 0 && slot < inv.size()) {
+					inv.set(slot, new ItemStack(tag));
 				}
 			}
 		}
 
 		@Override
 		public int getSizeInventory() {
-			return inv.length;
+			return inv.size();
 		}
 
 		@Override
 		public ItemStack getStackInSlot(int slot) {
-			if(slot >= inv.length)
+			if(slot >= inv.size())
 				return ItemStack.EMPTY;
 			
-			return inv[slot] == null || inv[slot].getItem() == Items.AIR ? ItemStack.EMPTY : inv[slot];
+			return inv.get(slot);
 		}
 
 		@Override
 		public ItemStack decrStackSize(int slot, int amt) {
-			ItemStack stack = inv[slot];
+			ItemStack stack = inv.get(slot);
 			if(stack != null) {
 				ItemStack stack2 = stack.splitStack(Math.min(amt, stack.getCount()));
 				if(stack.getCount() == 0)
-					inv[slot] = null;
+		            inv.set(slot, ItemStack.EMPTY);
 				
 				return stack2;
 			}
-			return null;
+			return ItemStack.EMPTY;
 		}
 
 		@Override
 		public void setInventorySlotContents(int slot, ItemStack stack) {
-			inv[slot] = stack;
+            inv.set(slot, stack);
 		}
 
 		@Override
@@ -99,7 +99,7 @@ public class EmbeddedInventory implements ISidedInventory {
 		@Override
 		public boolean isEmpty() {
 			for(ItemStack i : inv) {
-				if(i != null)
+				if(i != null && !i.isEmpty())
 					return false;
 			}
 			return true;
@@ -117,14 +117,14 @@ public class EmbeddedInventory implements ISidedInventory {
 
 		@Override
 		public boolean isItemValidForSlot(int slot, ItemStack item) {
-			return inv[slot] == null || (inv[slot].isItemEqual(item) && inv[slot].getMaxStackSize() != inv[slot].getCount());
+			return inv.get(slot).isEmpty() || (inv.get(slot).isItemEqual(item) && inv.get(slot).getMaxStackSize() != inv.get(slot).getCount());
 		}
 
 		@Override
 		public int[] getSlotsForFace(EnumFacing side) {
-			int array[] = new int[inv.length];
+			int array[] = new int[inv.size()];
 
-			for(int i = 0; i < inv.length; i++) {
+			for(int i = 0; i < inv.size(); i++) {
 				array[i] = i;
 			}
 			return array;
@@ -154,8 +154,8 @@ public class EmbeddedInventory implements ISidedInventory {
 
 		@Override
 		public ItemStack removeStackFromSlot(int index) {
-			ItemStack stack = inv[index];
-			inv[index] = null;
+			ItemStack stack = inv.get(index);
+			inv.set(index, ItemStack.EMPTY);
 			return stack;
 		}
 
